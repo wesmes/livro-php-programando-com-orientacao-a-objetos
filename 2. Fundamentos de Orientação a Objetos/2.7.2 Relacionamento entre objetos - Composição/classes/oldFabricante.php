b6a25d77bf7dc59d9fc03fee13007c285426d472
Weslley G. da Cruz <w_gomes@protonmail.com>
2023-02-15 12:17:20 -0300

    Finalização do exercício de composição.

---
+++ 2. Fundamentos de Orientação a Objetos/2.7.2 Relacionamento entre objetos - Composição/classes/oldFabricante.php
@@ -0,0 +1,24 @@
+<?php
+
+class Fabricante
+{
+
+    private $nome;
+    private $endereco;
+    private $documento;
+
+    public function __construct($nome, $endereco, $documento)
+    {
+
+        $this->nome      = $nome;
+        $this->endereco  = $endereco; 
+        $this->documento = $documento; 
+
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+}
